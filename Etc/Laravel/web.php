851f439f88efac5e9984a97ab05af01ac5d32b42
nemumixx <nemumixx@gmail.com>
2023-09-12 10:34:16 +0900

    php編集

--- Etc/Laravel/web.php
+++ Etc/Laravel/web.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppUserController;
 
 /*
 |--------------------------------------------------------------------------
