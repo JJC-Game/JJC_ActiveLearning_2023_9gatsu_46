ccf49f52ba86f8a9cd739e73091029812b68d9ce
nemumixx <nemumixx@gmail.com>
2023-09-12 12:09:00 +0900

    php編集

--- Etc/Laravel/play_chara_gacha.blade.php
+++ Etc/Laravel/play_chara_gacha.blade.php
@@ -1,1 +1,11 @@
-今回のガチャ結果は{{ $result_string }}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gacha Result</title>
+</head>
+<body>
+    <h1>今回のガチャ結果は{{ $result_string }}</h1>
+</body>
+</html>
